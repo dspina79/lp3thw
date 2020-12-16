56f79fe8f99344d01ca19054de6c9f639f5ebfb8
Dave Spina <dspina79@gmail.com>
2020-12-16 16:41:11 -0500

    Moved into folders and did basic math

---
+++ ex03/ex03-basicmath.py
@@ -0,0 +1,17 @@
+# Basic Math
+print("I will now count my chickens")
+print("Hens", 25 + 30 / 6) # should print 30
+print("Roosters", 100 - 25 * 3 % 4) # should print 97
+
+print("Now I will count the eggs:")
+print(3.0 + 2.0 + 1.0 - 5.0 - 4.0 % 2.0 - 1.0 / 4.0 + 6.0) # print should be 7 due to PEMDAS rules
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7) # should print false
+
+print("What is 3 + 2?", 3 + 2) # prints 5
+print("What is 5 - 7?", 5 - 7) # prints -2
+print("Oh, that's why it's false.")
+
+print("Is it greater?", 5 > -2) # prints true
+print("Is it greater or equal?", 5 >= -2) # prints true
+print("Is it less or equal?", 5 <= -2) # prints false
---
+++ ex03/ex03-challenge.py
@@ -0,0 +1,10 @@
+# Challenge/Study Drill for Basic Math
+
+# average 5 numbers
+print("The numbers are 3, 4, 5, 6, 7, 8")
+print("The average is about", (3 + 4 + 5 + 6 + 7 + 8)/5)
+
+# calculate temperature
+celcius = 15.0
+farenheit = (9.0/5.0) * celcius + 32.0
+print("15 degrees celcius in farenheit:", farenheit)
