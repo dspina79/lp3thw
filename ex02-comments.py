0b0d4a4ea2e37889a95d0032c1045f8cfef00478
Dave Spina <dspina79@gmail.com>
2020-12-16 16:01:55 -0500

    Addition of the comments exercise

---
+++ ex02-comments.py
@@ -0,0 +1,12 @@
+# Comments
+
+# this is a comment line.
+# this will not run
+
+print("But this will run")
+
+#print("Yet this will not")
+
+print("I think comments are great")  # and so does this inline one
+
+print("Goodnight, Moon!")
