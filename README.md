7d6a6f484da6717ea3d8f1d02d6936e35122053d
Dave Spina <dspina79@gmail.com>
2020-12-16 13:27:35 -0500

    Added exercise 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# lp3thw
-Learn Python the hard way courses.
+# Learn Python the Hard Way
+Learn Python the hard way course exercises, notes, and additional learning items.
+
+Starting with https://learnpythonthehardway.org/python3/ex0.html.
---
+++ ex01-printing.py
@@ -0,0 +1,12 @@
+# Exercise 1
+# A Good First Program
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun")
+print('Yay! This is printing')
+print("I'd much rahter your 'not'.")
+print('I "said" do not touch this.')
+#print("This should be commented.")
+print("I am always willing to start from the\nbeginning if this is going to help\nothers learn.")
