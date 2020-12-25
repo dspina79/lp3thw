2ddcecb128dc9f1abbdab35c8c740772978d218c
Dave Spina <dspina79@gmail.com>
2020-12-24 21:09:43 -0500

    Handling inputs through exercise 12

---
+++ ex12/ex12-prompting.py
@@ -0,0 +1,6 @@
+# Asking Questions with Prompts
+
+age = input("How old are you? ")
+height = input("How tall are you, in inches? ")
+weight = input("How much do you weigh, in pounds? ")
+print(f"So you are {age} years old, are {height} inches tall, are {weight} pounds.")
