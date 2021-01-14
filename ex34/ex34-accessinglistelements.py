066ed768921a65657cfc952ead8fc48011eea65a
Dave Spina <dspina79@gmail.com>
2021-01-14 01:02:49 -0500

    Included exercise 34 on lists and list access

---
+++ ex34/ex34-accessinglistelements.py
@@ -0,0 +1,12 @@
+# Accessing List Elements
+
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(f"The animal at 1 is {animals[1]}.")
+print(f"The third (3rd) animal is {animals[2]}.")
+print(f"The first (1st) animal is {animals[0]}.")
+print(f"The animal at 3 is  {animals[3]}.")
+print(f"The fifth (5th) animal is {animals[4]}.")
+print(f"The animal at 2 is  {animals[2]}.")
+print(f"The sixth (6th) animal is {animals[5]}.")
+print(f"The animal at 4 is {animals[4]}.")
