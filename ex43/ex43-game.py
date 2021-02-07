f54194dcb0a34d539ebd8dd85d40004d1945ac00
Dave Spina <dspina79@gmail.com>
2021-02-06 20:47:32 -0500

    Applied basic game engine mechanics

--- ex43/ex43-game.py
+++ ex43/ex43-game.py
@@ -5,22 +5,31 @@ from textwrap import dedent
 
 class Scene(object):
     def enter(self):
-        pass
+        print("You came up in the void. This is impossible.")
+        exit(1)
 
 class Engine(object):
     def  __init__(self, scene_map):
-        pass
+        self.game_map = map
     
     def play(self):
-        pass
+        while True:
+            self.game_map.opening_scene().enter()
+            self.next_scene('')
 
 class Death(Scene):
+    def __init__(self):
+        super().__init__()
+        self.cause = 'You died.'
+
     def __init__(self, cause):
         super().__init__()
         self.cause = cause
 
     def enter(self):
-        pass
+        print(self.cause)
+        print("--- GAME OVER ---")
+        exit(1)
 
 class CentralCorridor(Scene):
     def enter(self):
@@ -68,17 +77,32 @@ class EscapePod(Scene):
 
 
 class Map(object):
+
+    scenes = ['CentralCorridor', 'LaserWeaponsArmory', 'Bridge', 'EscapePod', 'Death']
+    current_scene_index = 0
     def __init__(self, start_scene):
-        self.scene = opening_scene
+        self.current_scene_index = 0
 
     def next_scene(self, scene_name):
-        self.scene = get_scene(scene_name)
+        if scene_name != '':
+            self.current_scene_index = self.scenes.index(scene_name)
+        else:
+            self.current_scene_index += 1
 
     def opening_scene(self):
-        return CentralCorridor()
+        return get_scene(self.scenes[self.current_scene_index])
 
     def get_scene(self, scene_name):
-        pass
+        if scene_name == 'CentralCorridor':
+            return CentralCorridor()
+        elif scene_name == 'LaserWeaponsArmory':
+            return LaserWeaponsArmory()
+        elif scene_name == 'Bridge':
+            return TheBridge()
+        elif scene_name == 'EscapePod':
+            return EscapePod()
+        else:
+            return Death('You didn\'t make it. You died.')
 
 def main():
     a_map = Map('central_corridor')
